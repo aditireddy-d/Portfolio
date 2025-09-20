a040cbc75f6212d3e7609d7519a02b1057d6fcfb
aditi220403 <ad7977@g.rit.edu>
2025-09-20 12:58:17 -0400

    Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio/' : '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: mode === "production" ? "/Portfolio/" : "/",
+  server: { host: "::", port: 8080 },
+  build: { outDir: "docs", emptyOutDir: true },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
