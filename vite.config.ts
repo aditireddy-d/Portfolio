52bf54a7404e36519a2ceaec2cb4e541f34507b8
aditi220403 <ad7977@g.rit.edu>
2025-09-20 13:15:56 -0400

    Add docs build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Portfolio/" : "/",
+  // base: mode === "production" ? "/Portfolio/" : "/",
+  base: "/Portfolio/",
   server: { host: "::", port: 8080 },
   build: { outDir: "docs", emptyOutDir: true },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
