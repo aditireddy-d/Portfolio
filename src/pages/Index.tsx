dfb87315fc9a520121a0efbf60d2a21ffc7dfc72
aditi220403 <ad7977@g.rit.edu>
2026-01-20 10:05:48 -0500

    Fix main page background to black for dark theme

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import ContactSection from "@/components/ContactSection";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-black">
       <Navigation />
       <HeroSection />
       <AboutSection />
